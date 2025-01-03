34c95cb8d832c6e8fd9a427a8587566f2e564fc0
Joao vitor <jvzinn2074@gmail.com>
2025-01-02 21:01:14 -0300

    estudando java script

---
+++ aulas/aula15/aula.js
@@ -0,0 +1,39 @@
+const converterint =(e) => parseInt(e)
+
+const dobrar = (e)=>e*2
+
+let num = ['1', '2','3','4', '5'].map(dobrar)
+console.log(num)
+
+
+
+// let el= document.getElementsByTagName("div")
+// const val = Array.prototype.map.call(el,({innerHTML})=> innerHTML)
+// console.log(val)
+
+
+
+
+
+
+
+
+
+// let el=document.getElementsByTagName("div")
+// el = [...el]
+// el.map((e,i)=>{
+//     console.log(e.innerHTML="JV")
+// })
+// console.log(el)
+
+
+
+
+
+// const cursos = ['HTML', 'CSS', 'Javascript', 'PHP', 'React']
+// let c = cursos.map((el,i) => {
+//     return el 
+    
+// })
+
+// console.log(c)
