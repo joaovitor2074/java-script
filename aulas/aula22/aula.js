7a049a7a1858c856963af1e196f44dac66800d0e
Joao vitor <jvzinn2074@gmail.com>
2025-01-08 15:56:39 -0300

    estudando

--- aulas/aula22/aula.js
+++ aulas/aula22/aula.js
@@ -1,12 +1,21 @@
 const caixa1 = document.querySelector("#caixa1")
 const btn_c = document.querySelectorAll(".curso")
+const c1_2 = document.querySelector("#c1_2")
 
 
-console.log(caixa1.hasChildNodes())
-console.log(btn_c[0].hasChildNodes())
-console.log(btn_c[0].childNodes)
+console.log(c1_2.parentNode.parentNode.children[4])
 
-console.log (caixa1.children.length >0 ? "possui filhos" : "nao possui filhos")
+
+
+
+// console.log(caixa1.hasChildNodes())
+// console.log(btn_c[0].hasChildNodes())
+// console.log(btn_c[0].childNodes)
+
+// console.log (btn_c[0].children.length >0 ? "possui filhos" : "nao possui filhos")
+
+
+// console.log(caixa1.children[1].innerHTML="jv")
 
 
 
