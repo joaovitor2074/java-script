3ef11a37fda4d77e28d734d51b3a3400d27276fd
Joao vitor <jvzinn2074@gmail.com>
2025-01-12 08:40:41 -0300

    estuidando

--- aulas/aula22/aula.js
+++ aulas/aula22/aula.js
@@ -8,6 +8,15 @@ cursos.map((el,chave)=>{
     novoelemento.setAttribute("id", "c" + chave)
     novoelemento.setAttribute("class","curso c1")
     novoelemento.innerHTML= el
+    novoelemento.appendChild(btn_lixeira)
+    
+    const btn_lixeira = document.createElement("img")
+    btn_lixeira.setAttribute("src","./lixeira.png")
+    btn_lixeira.setAttribute("class", "btn_lixeira")
+    btn_lixeira.addEventListener("click", (evt)=>{
+        
+        caixa1.removeChild(evt.target.parentNode)
+    })
     caixa1.appendChild(novoelemento)
 })
 
