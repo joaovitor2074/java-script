3f18ef5f6a4324167f2d19467096f62203771b6c
Joao vitor <jvzinn2074@gmail.com>
2025-01-26 10:11:39 -0300

    inicio de estudos sobre objetos

---
+++ aulas/aula34/aula.js
@@ -0,0 +1,13 @@
+class Pessoa{
+    constructor(nome){
+        this.nome = nome;
+    }
+}
+
+let p1 = new Pessoa("jvzin")
+let p2 = new Pessoa("dayane")
+let p3 = new Pessoa("sueide")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
