983b4a3435c749c36a1b5a74d9bc64c6e7c0bea6
Joao vitor <jvzinn2074@gmail.com>
2025-01-27 13:59:37 -0300

    uptade aula.js

--- aulas/aula37/aula.js
+++ aulas/aula37/aula.js
@@ -1,24 +1,20 @@
-function pessoa (nome,idade){
-        this.nome = nome,
-        this.idade = idade,
-
-this.info=function(){
-        return `nome: ${this.nome} <br/>
-        idade: ${this.idade} <br/>
-        -------------------------------------------------------------- `
+class pessoa{
+    constructor(nome,idade){
+        this.nome = nome
+        this.idade = idade
     }
-}
-
+    info(){
+        return `nome: ${this.nome} <br/>
+                idade: ${this.idade}<br/>
+                ---------------------------------------------------------------- `
+    }}
 const pessoas = []
-
-let nome = document.querySelector("#f_form")
-let idade = document.querySelector("#f2_form")
+let f1 = document.querySelector("#f_form")
+let f2 = document.querySelector("#f2_form")
 let btn = document.querySelector("#btn_add")
-let res = document.querySelector("#pres")
-
+let res =  document.querySelector("#pres")
 btn.addEventListener("click",(evt)=>{
-    let p1 = new pessoa(nome.value,idade.value)
+    let p1 = new pessoa(f1.value,f2.value)
     pessoas.push(p1)
-    console.log(pessoas)
-    res.innerHTML =pessoas.map(p => p.info())
+    res.innerHTML = pessoas.map(p => p.info()).join(" ")
 })
