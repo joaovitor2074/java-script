95f57b44c36b9ff78a690de689bb7b529402d15a
Joao vitor <jvzinn2074@gmail.com>
2025-01-29 16:42:14 -0300

    estudando sobre static

---
+++ aulas/aula41/aula.js
@@ -0,0 +1,21 @@
+class npc{
+    static alerta = false
+    constructor(energia){
+        this.energia = energia
+    }
+    info=function(){
+        console.log(`energia: ${this.energia}`)
+        console.log(`Alerta: ${(npc.alerta?"sim":"nao")}`)
+    }
+
+    static alertar = function(){
+        npc.alerta=true
+    }
+}
+
+const npc1 = new npc(100)
+
+npc.alerta  = true
+
+
+npc1.info()
