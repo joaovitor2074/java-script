2b69618c5c6c5ed5c966453e192b42410bee0421
Joao vitor <jvzinn2074@gmail.com>
2025-01-29 15:56:24 -0300

    estudando

---
+++ aulas/aula40/aula.js
@@ -0,0 +1,17 @@
+const pessoa = {
+    nome:"bruno",
+    canal:"curso em video",
+    cursos:"javascript",
+    aulas: {
+        aula1:"introducao",
+        aula2:"sintaxe",
+        aula3:"logica"
+    }
+}
+const stringpessoa = '{"nome":"bruno","canal":"curso em video","cursos":"javascript","aulas":{"aula1":"introducao","aula2":"sintaxe","aula3":"logica"}}'
+
+const s_json = JSON.stringify(pessoa)
+const o_json = JSON.parse(stringpessoa)
+console.log(pessoa)
+console.log(s_json)
+console.log(o_json)
