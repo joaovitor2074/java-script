dbafa2a23be287b490a53a5b80362f08dd93d354
Joao vitor <jvzinn2074@gmail.com>
2025-02-03 15:01:45 -0300

    estudando

--- aulas/aula43/aula.js
+++ aulas/aula43/aula.js
@@ -2,14 +2,62 @@ const teclasnum = [...document.querySelectorAll(".num")]
 const teclasop = [...document.querySelectorAll(".op")]
 const teclasres = document.querySelector(".res")
 const display = document.querySelector("#display")
+const limpar = document.getElementById("tlimpar")
+const virgula = document.getElementById("separador")
+const igual = document.getElementById("tigual")
+
+let sinal = false
+let decimal = false;
+
+
 
 teclasnum.forEach((el)=>{
     el.addEventListener("click", (evt)=>{
-        display.innerHTML += evt.target.innerHTML
+        sinal = false
+        if(evt.target.innerHTML==","){
+            if(!decimal){
+                decimal = true
+                if(display.innerHTML =="0"){
+                    display.innerHTML = "0"
+                }
+                display.innerHTML+=evt.target.innerHTML
+            }
+            
+        }else{
+            display.innerHTML += evt.target.innerHTML
+            if(display.innerHTML =="0"){
+                display.innerHTML = ""
+            }
+        }
+
     })
 })
 teclasop.forEach((el)=>{
     el.addEventListener("click", (evt)=>{
-        display.innerHTML += evt.target.innerHTML
+        if(!sinal){
+            sinal=true
+            if(display.innerHTML =="0"){
+                display.innerHTML = ""
+            }
+
+            if(evt.target.innerHTML=="x"){
+                display.innerHTML +="*"
+            }else{
+                display.innerHTML += evt.target.innerHTML
+            }
+        }
+        
     })
+})
+
+limpar.addEventListener("click", (evt)=>{
+    display.innerHTML = ""
+    sinal = false
+    decimal = false
+})
+igual.addEventListener("click", (evt)=>{
+    sinal = false
+    decimal = false
+    const res = eval(display.innerHTML)
+    display.innerHTML = res
 })
