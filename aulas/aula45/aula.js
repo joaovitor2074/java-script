01fd375263af69df514f330cbacb93e78e380eea
Joao vitor <jvzinn2074@gmail.com>
2025-02-05 10:34:11 -0300

    crinado relogio

--- aulas/aula45/aula.js
+++ aulas/aula45/aula.js
@@ -1,9 +1,10 @@
 const data = new Date()
 const  divdata = document.getElementById("divdata")
+const relogio =  document.getElementById("relogio")
 
 // console.log(Date.now())
 
-let mes = data.getMonth()
+let mes = data.getMonth() + 1
 mes = mes<10?"0" + mes:mes
 console.log(mes)
 
@@ -18,3 +19,17 @@ if(data.getDate() <10){
     divdata.innerHTML  = datas
     console.log(datas)
 }
+
+const relogios = ()=>{
+    const data = new Date()
+    let hora = data.getHours()
+    hora = hora<10? "0" + hora: hora
+    let minuto = data.getMinutes()
+    minuto = minuto<10? "0" + minuto: minuto
+    let segundos = data.getSeconds()
+    segundos = segundos <10?"0" + segundos:segundos
+    relogio.innerHTML = hora + ":" + minuto + ":" + segundos
+}
+
+const intervalo = setInterval(relogios,1000)
+
