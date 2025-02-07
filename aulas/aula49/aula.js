c747481e91dac78399624d2f905c1e15bafd28ec
Joao vitor <jvzinn2074@gmail.com>
2025-02-07 10:20:44 -0300

    estudando sobre historico

---
+++ aulas/aula49/aula.js
@@ -0,0 +1,15 @@
+const url = document.getElementById("url")
+btn_url  =document.getElementById("btn_url")
+
+btn_url.addEventListener("click",(evt)=>{
+    // window.location = url.value
+    // window.location.replace("https://github.com/joaovitor2074")
+    // window.location.assign("https://github.com/joaovitor2074")
+    // window.location.reload()'
+    //window.history.back()
+    // window.history.forward()
+    // window.history.go(1)
+    
+
+    console.log(window.history.length)
+})
