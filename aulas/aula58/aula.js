146c17d27058ff558ecc78c7c967952df3747980
Joao vitor <jvzinn2074@gmail.com>
2025-02-13 16:05:45 -0300

    estudando sobre string

--- aulas/aula58/aula.js
+++ aulas/aula58/aula.js
@@ -1,7 +1,26 @@
-let nome = new String("bruno")
+let nome = new String("joao vitor salazar")
+let nome2 = new String("bruno")
 
 
 // console.log(nome.charAt(1))
 // console.log(nome.charCodeAt(1))
 // console.log(nome.concat("jv"))
-console.log(nome.lastIndexOfndexOf("u"))
+// console.log(nome.lastIndexOfndexOf("u"))
+// console.log(nome.localeCompare(nome2))
+// console.log(nome.replace("b","p"))
+// console.log(nome.search("r"))
+// console.log(nome.slice(1,4))
+ let arr = nome.split(" ")
+//  console.log(arr)
+
+// let parte  =nome.substring(5,11)
+// console.log(parte)
+// let parte1  =nome.substr(6,5)
+// console.log(parte1)
+
+// console.log(nome.toLocaleUpperCase())
+// console.log(nome.toLowerCase())
+
+// console.log(nome.valueOf())
+let num = 10
+console.log(typeof)
