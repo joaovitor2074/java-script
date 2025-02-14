5fc7bc1943835c02d5004f9c96f11570b525bbe8
Joao vitor <jvzinn2074@gmail.com>
2025-02-14 15:54:14 -0300

    finalizando estudos sobre string

---
+++ aulas/aula60/aula.js
@@ -0,0 +1,21 @@
+let nome = new String("joaooooooo vitorrrrrrrr salazar 2024")
+let gmail = "jvzinn2074@gmail.com"
+let numeros = "1,10,100,1000"
+
+// console.log(nome.search(/joao/i))
+
+// console.log(nome.match(/o/ig))
+
+// console.log(nome.replace(/Joao/i, /Jv/))
+
+// console.log(nome.match(/[oi]/ig))
+// console.log(nome.match(/[a-g|h-z|0-9]/ig))
+
+// console.log(nome.match(/\d/ig))
+// console.log(nome.match(/\s/ig))
+// console.log(nome.match(/\bi/ig))
+
+
+// console.log(nome.match(/o+|r+/ig))
+// console.log(numeros.match(/10*/ig))
+console.log(numeros.match(/10?/ig))
