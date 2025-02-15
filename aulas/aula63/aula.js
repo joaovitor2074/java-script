5949a3de17f4523c30d93c8cb87aeba6c66f9830
Joao vitor <jvzinn2074@gmail.com>
2025-02-14 21:32:44 -0300

    estudando sobre negocio que esqueci o nome

---
+++ aulas/aula63/aula.js
@@ -0,0 +1,22 @@
+const q1 = document.getElementById('q1');
+const q2 = document.getElementById('q2');
+const posx = document.getElementById('posx');
+const posy = document.getElementById('posy');
+const largura = document.getElementById('largura');
+const altura = document.getElementById('altura');
+
+
+q1.addEventListener("click",(evt)=>{
+    let DOMRect_q1 = q1.getBoundingClientRect()
+    posx.innerHTML = `posx : ${DOMRect_q1.x}`
+    posy.innerHTML = `posy : ${DOMRect_q1.y}`
+    largura.innerHTML = `largura : ${DOMRect_q1.width}`
+    altura.innerHTML = `posy : ${DOMRect_q1.height}`
+})
+q2.addEventListener("click",(evt)=>{
+    let DOMRect_q2 = q2.getBoundingClientRect()
+    posx.innerHTML = `posx : ${DOMRect_q2.x}`
+    posy.innerHTML = `posy : ${DOMRect_q2.y}`
+    largura.innerHTML = `largura : ${DOMRect_q2.width}`
+    altura.innerHTML = `posy : ${DOMRect_q2.height}`
+})
