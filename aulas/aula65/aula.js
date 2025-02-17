b0fc4bac91a87ada950ec6e51c354b19e0f0995e
Joao vitor <jvzinn2074@gmail.com>
2025-02-17 14:07:54 -0300

    estudandoo

---
+++ aulas/aula65/aula.js
@@ -0,0 +1,18 @@
+const long = document.getElementById("long")
+const lati = document.getElementById("lati")
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(mostrarlocalizacao,errolocalizacao)
+    
+
+}else{
+    console.log("geogolocalizacao nao suportada")
+}
+function mostrarlocalizacao(pos){
+    long.innerHTML = pos.coords.longitude
+    lati.innerHTML = pos.coords.latitude
+}
+function errolocalizacao(){
+    console.log("erro ao obter a localizacao")
+}
+ 
