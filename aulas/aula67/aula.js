ad3fc51cefac471834aaac6df43f7b4b2ab72218
Joao vitor <jvzinn2074@gmail.com>
2025-02-18 10:10:42 -0300

    estudadno sobre web storage

---
+++ aulas/aula67/aula.js
@@ -0,0 +1,14 @@
+const f_texto = document.querySelector("#f_texto")
+const p_texto = document.querySelector("#p_texto")
+const btn_texto = document.getElementById("btn_texto")
+
+
+btn_texto.addEventListener("click",(evt)=>{
+
+})
+
+let num = 10
+localStorage.setItem("numero" ,num)
+console.log(localStorage.getItem("numero"))
+console.log(localStorage.key(0))
+localStorage.
