226c24fb37f29efde5644a3a2d689f59e4bb8682
Joao vitor <jvzinn2074@gmail.com>
2025-08-23 18:30:35 -0300

    estudando java script

--- testes/oqaprendi.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("btn");
-const res = document.getElementById("res");
-
-btn.addEventListener("click", () => {
-    const input = document.getElementById("input").value; // Obtém o valor do input
-    let regex = /@gmail\.com$/; // Expressão regular para verificar se termina com "@gmail.com"
-    console.log(regex.test(input)); // Verifica se a string termina com "@gmail.com"
-});
