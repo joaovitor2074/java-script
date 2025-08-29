8a0782d8a4d14b0bdf0a8a3c319ae2b3c931a965
Joao vitor <jvzinn2074@gmail.com>
2025-08-29 18:13:29 -0300

    estudando

--- agenda/app.js
+++ agenda/app.js
@@ -16,5 +16,27 @@ app.get("/contato",async(req,res)=>{
     res.status(500).json({ erro: error.message });
     }
 })
+
+app.post("/addcontatos", async (req, res) => {
+    const { f_nome, f_celular, f_email, f_datanascimento } = req.body
+
+    console.log("Recebi do front:", req.body)
+
+    try {
+        await db.query(
+            "INSERT INTO contato (s_nome_conato, s_celular_contato, s_email_contato, f_dtnas) VALUES (?, ?, ?, ?)",
+            [f_nome, f_celular, f_email, f_datanascimento]
+        )
+        res.status(200).json({ msg: "Contato gravado com sucesso!" })
+    } catch (error) {
+        console.error("Erro ao inserir:", error.sqlMessage || error.message)
+        res.status(500).json({ erro: error.message })
+    }
+})
+
+
+
+
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
---
+++ agenda/routes/novo.js
@@ -0,0 +1,43 @@
+const btn_gravar = document.getElementById("btn_gravar")
+const btn_cancelar = document.getElementById("btn_cancelar")
+const f_nome = document.getElementById("f_nome")
+const f_celular = document.getElementById("f_celular")
+const f_email = document.getElementById("f_email")
+const f_datanascimento = document.getElementById("f_datanascimento")
+
+btn_gravar.addEventListener("click", (evt) => {
+    evt.preventDefault(); // evita recarregar a página caso esteja dentro de um <form>
+
+    const valores = {
+        f_nome: f_nome.value,
+        f_celular: f_celular.value,
+        f_email: f_email.value,
+        f_datanascimento: f_datanascimento.value
+    }
+
+    const cabecalho = {
+        method: "POST", // estava escrito "Mathod"
+        headers: {
+            "Content-Type": "application/json" // necessário pro back entender JSON
+        },
+        body: JSON.stringify(valores) // aqui vai o JSON dos dados
+    }
+
+    const endpoint = "http://localhost:3000/addcontatos"
+
+   fetch(endpoint, cabecalho)
+    .then(res => {
+        if (!res.ok) {
+            throw new Error("Erro na resposta do servidor: " + res.status)
+        }
+        return res.json()
+    })
+    .then(data => {
+        console.log("✅ Resposta do backend:", data)
+    })
+    .catch(err => {
+        console.error("❌ Erro de conexão ou gravação:", err)
+    })
+})
+
+
